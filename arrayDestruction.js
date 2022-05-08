1df576cc2a3d3ade9b795dc1ee90eee9df6525ac
Vaishnavi <vaishnavivhora5@gmail.com>
2022-05-09 05:06:25 +0530

    function on array object

---
+++ arrayDestruction.js
@@ -0,0 +1,6 @@
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+ var [name, skills, scores] = student
+ var [[], [], [, , jsScore, reactScore]] = student
+ console.log(name, skills, scores)
+ console.log(jsScore, reactScore)
+
