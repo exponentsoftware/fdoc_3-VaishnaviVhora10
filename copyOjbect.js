80d4b6fec5ba905023fd73f0bed505a3ae77df39
Vaishnavi <vaishnavivhora5@gmail.com>
2022-05-09 04:33:05 +0530

    object cpoied to new object and edited

---
+++ copyOjbect.js
@@ -0,0 +1,44 @@
+// const person = {
+//     firstName: 'John',
+//     lastName: 'Doe'
+// };
+// // using spread ...
+// let p1 = {
+//     ...person
+// };
+
+const student = {
+    name: 'David',
+    age: 25,
+    skills: {
+        frontEnd: [
+            { skill: 'HTML', level: 10 },
+            { skill: 'CSS', level: 8 },
+            { skill: 'JS', level: 8 },
+            { skill: 'React', level: 9 }
+        ],
+        backEnd: [
+            { skill: 'Node',level: 7 },
+            { skill: 'GraphQL', level: 8 },
+        ],
+        dataBase:[
+            { skill: 'MongoDB', level: 7.5 },
+        ],
+        dataScience:['Python', 'R', 'D3.js']
+    }
+}
+
+// console.log("student data",student)
+
+const newStudent = {
+    ...student,
+    skills: {
+        ...student.skills,
+        frontEnd: [...student.skills.frontEnd, { skill: 'Bootstrap', level: 8 }],
+        backEnd: [...student.skills.backEnd, { skill: 'Express', level: 8 }],
+        dataBase: [...student.skills.dataBase, { skill: 'SQL', level: 8 }],
+        dataScience: [...student.skills.dataScience, { skill: "SQL" }],
+
+    }
+};
+console.log(newStudent);
