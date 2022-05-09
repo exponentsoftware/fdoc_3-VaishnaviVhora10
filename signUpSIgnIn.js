91b6b46677914728b12d293f902414fa5678bb9a
Vaishnavi <vaishnavivhora5@gmail.com>
2022-05-09 06:08:54 +0530

    function for signupand signin

---
+++ signUpSIgnIn.js
@@ -0,0 +1,102 @@
+const users = [
+    {
+            _id: 'ab12ex',
+            username: 'Alex',
+            email: 'alex@alex.com',
+            password: '123123',
+            createdAt:'17/05/2019 9:00 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'fg12cy',
+            username: 'Asab',
+            email: 'asab@asab.com',
+            password: '123456',
+            createdAt:'17/05/2019 9:30 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'zwf8md',
+            username: 'Brook',
+            email: 'brook@brook.com',
+            password: '123111',
+            createdAt:'17/05/2019 9:45 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'eefamr',
+            username: 'Martha',
+            email: 'martha@martha.com',
+            password: '123222',
+            createdAt:'17/05/2019 9:50 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'ghderc',
+            username: 'Thomas',
+            email: 'thomas@thomas.com',
+            password: '123333',
+            createdAt:'17/05/2019 10:00 AM',
+            isLoggedIn: false
+    }
+    ];
+
+    const products = [
+{
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+        { userId: 'fg12cy', rate: 5 },
+        { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+},
+{
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+},
+{
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+}
+];
+
+signUp(users, {
+    _id: 'ghdercfc',
+    username: 'Natasha',
+    email: 'nora@thomas.com',
+    password: '123333',
+    createdAt:'17/05/2019 12:00 AM',
+    isLoggedIn: true
+});
+
+function signUp(db, user) {
+    const existingUser = db.some(({ username }) => username === user.username);
+    if (existingUser) throw "user already exists";
+    db.push(user);
+}
+
+signIn(users, 'Vaish', '123222');
+
+function signIn(db, username, password) {
+    for (let i = 0; i < db.length; i++){
+        if (db[i].username === username && db[i].password === password) {
+            db[i].isLoggedIn = true;
+            console.log('logged in');
+            return true;
+        };
+    }
+    return false;
+}
+
+// console.log(users);
